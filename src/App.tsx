144a23636e4bbabb80e18a28f5df7d66741a54ee
NahuelMer <ariel.nahuel29@gmail.com>
2022-05-17 14:32:24 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { HooksPlayers } from './players';
+// import { HookBoard } from './board'
+import React, { useState, useEffect } from "react"
 
 function App() {
+
+  const [playersComponent, setPlayersComponent] = useState(true);
+  const [showButton, setShowButton] = useState(true);
+
+  let showPlayersComponent = () => {
+    setPlayersComponent(!playersComponent)
+    setShowButton(!showButton)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>Ta te ti</h1>
+        <div>
+        {/* {playersComponent ? <HooksPlayers /> : <HookBoard />} */}
+        <br />
+        {showButton ? <input type="submit" id="playButton" name="sub1" value="Jugar" onClick={showPlayersComponent}></input> : null}
+        </div>
       </header>
+
     </div>
   );
 }
